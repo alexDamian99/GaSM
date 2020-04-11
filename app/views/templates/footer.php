8f366a7d91cdb387d8fdc68d48dadb8eccd93f35
Stefan Beleuz <stefan.beleuz@yahoo.com>
2020-04-11 14:16:14 +0300

    mvc

---
+++ app/views/templates/footer.php
@@ -0,0 +1,65 @@
+<footer>
+        <div id="top-footer">
+            <div id="footer-info">
+                <p id="footer-title">GaSM<br />Garbage Smart Monitor</p>
+                <span>Contact us</span>
+                <p id="footer-contact">
+                    <i class="fa fa-envelope-o"></i> info@gasm.com<br />
+                    <i class="fa fa-phone"></i> +40000000000
+                </p>
+            </div>
+
+            <div id="footer-helpful">
+                <span>Helpful links</span>
+                <ul>
+                    <li>
+                        <a href="#">Why to recycle</a>
+                    </li>
+
+                    <li>
+                        <a href="#">How to recycle</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Recycle impact</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div id="footer-vision">
+                <span>Our vison</span>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in arcu augue. Nulla enim mauris, aliquet ut lorem aliquet, vestibulum elementum est.
+                </p>
+            </div>
+        </div>
+
+        <div id="low-footer">
+            <div id="footer-social">
+                <span>Social</span>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-facebook-official" style="font-size:1.25em; color: #e6efec;"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-twitter-square" style="font-size:1.25em; color: #e6efec;"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-instagram" style="font-size:1.25em; color: #e6efec;"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <p id="footer-copyright">©GaSM 2020</p>
+
+            <a class="footer-logo-container" href="#">
+                <img id="footer-logo" src="assets/images/logo_footer.png" alt="logo" />
+            </a>
+        </div>
+</footer>
