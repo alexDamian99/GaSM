f0ea0dcb843c2c459b0ce74bdeab0fcad38624ca
StefanBeleuz <stefan.beleuz@yahoo.com>
2020-05-14 18:50:17 +0300

    reset password

--- app/controllers/forgotPass.php
+++ app/controllers/forgotPass.php
@@ -3,18 +3,110 @@ session_start();
 
 class ForgotPass extends Controller
 {
+    private $model;
+
     public function __construct()
     {
         parent::__construct();
-        $this->view('login/forgotPass', []);
+
+        $this->model = $this->model('LoginModel');
     }
 
     public function index()
     {
+        $text = 'Enter your email adress and follow the steps to recover your account.';
+        $this->view('login/forgotPass', ['text' => $text, 'input-type' => 'email']);
+
         if (isset($_POST['submit'])) {
-            $email = $_POST['email'];
-            // send email
-            $this->redirect('signin');
+            $to = $_POST['email'];
+            if (!$this->model->checkEmail($to)) {
+                $_SESSION['reset-email'] = $to;
+                $this->sendEmail($to, 'GaSM - Reset your password', $this->generateToken());
+                $this->redirect('forgotPass/token');
+            } else {
+                $_SESSION['error'] = 'Email not found!';
+                header("Refresh:0");
+            }
         }
+    }
+
+    public function token()
+    {
+        $text = 'Enter the token received on the email.';
+        $this->view('login/forgotPass', ['text' => $text, 'input-type' => 'token']);
+
+        if (isset($_POST['submit'])) {
+            $token = $_POST['token'];
+            if ($this->model->checkToken($_SESSION['reset-email'], $token)) {
+                $this->redirect('./reset/' . $token);
+            } else {
+                $_SESSION['error'] = 'Incorrect token!';
+                header("Refresh:0");
+            }
+        }
+    }
+
+    public function reset($params)
+    {
+        $token = $params[0];
+        $text = 'Enter your new password.';
+        $this->view('login/forgotPass', ['text' => $text, 'input-type' => 'password']);
+
+        if (isset($_POST['submit'])) {
+            $password = $_POST['password'];
+            if ($this->checkPassword($password) != 'Very weak') {
+                $this->model->updatePassword($_SESSION['reset-email'], md5($password));
+                unset($_SESSION['reset-email']);
+                $this->redirect('../../signin');
+            } else {
+                $_SESSION['error'] = 'Password is too weak';
+                header("Refresh:0");
+            }
+        }
+    }
+
+    /* util funs */
+
+    public function sendEmail($to, $subject, $message)
+    {
+        $headers = array(
+            'From' => 'pythonsender77@gmail.com',
+            'Reply-To' => 'pythonsender77@gmail.com',
+            'X-Mailer' => 'PHP/' . phpversion()
+        );
+
+        mail($to, $subject, $message, $headers);
+    }
+
+    public function generateToken()
+    {
+        $token = '';
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        for ($i = 0; $i < 4; $i++) {
+            $token .= $characters[rand(0, $charactersLength - 1)];
+        }
+
+        $this->model->insertToken($_SESSION['reset-email'], $token);
+        return $token;
+    }
+
+    public function checkPassword($password)
+    {
+        $rez = "Very weak";
+
+        if (preg_match("/^(?=.*\d)(?=.*[a-z]).{6,}$/m", $password))
+            $rez = "Weak";
+
+        if (
+            preg_match("/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/m", $password) ||
+            preg_match("/^(?=.*\d)(?=.*[a-z])(?=.*[^a-zA-Z0-9]).{6,}$/m", $password)
+        )
+            $rez = "Good";
+
+        if (preg_match("/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/m", $password))
+            $rez = "Strong";
+
+        return $rez;
     }
 }
--- app/views/login/forgotPass.php
+++ app/views/login/forgotPass.php
@@ -4,28 +4,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../public/assets/css/forgotPass.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo getenv("path_to_public"); ?>/assets/css/forgotPass.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="<?php echo getenv("path_to_public"); ?>/assets/js/register.js"></script>
     <title>GaSM | Recover account</title>
 </head>
 
 <body>
     <main>
         <form action="" method="POST">
-            <a class="logo-container" href="./index">
-                <img id="logo" src="../public/assets/images/navbar_logo.png" alt="logo">
+            <a class="logo-container" href=<?php echo getenv("path_to_public"); ?>>
+                <img id="logo" src="<?php echo getenv("path_to_public"); ?>/assets/images/navbar_logo.png" alt="logo">
             </a>
 
             <h1>Recover account</h1>
 
-            <p>Enter your email adress and follow the steps to recover your account.</p>
+            <p><?php echo $data['text'] ?></p>
 
             <label>
-                <input class="input" type="email" name="email" placeholder="Email" required value="<?php if (isset($_COOKIE["temp_email"])) {
-                                                                                                        echo $_COOKIE["temp_email"];
-                                                                                                    } ?>">
+                <?php
+                if ($data['input-type'] == 'email')
+                    echo '<input class="input" type="email" name="email" placeholder="Email" required>';
+                else if ($data['input-type'] == 'token')
+                    echo '<input class="input" type="text" name="token" placeholder="Token" required>';
+                else if ($data['input-type'] == 'password')
+                    echo '<input class="input" type="password" name="password" placeholder="New Password"
+                    onkeyup="checkPassStrength(this.value)" required>'
+                ?>
             </label>
+
+            <span id="pass-strength" class="tooltip"></span>
+
+            <span class="display-errors"> <?php if (isset($_SESSION['error'])) {
+                                                echo $_SESSION['error'];
+                                                unset($_SESSION["error"]);
+                                            } ?> </span>
 
 
             <button type="submit" name="submit">Submit</button>
