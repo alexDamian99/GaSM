91717a0f8a6f8d0d51249578cfd367b4e5736a1d
Andrei Ianau <aianau@bitdefender.com>
2020-05-21 20:19:02 +0300

    removed adding yet another page to pdf

--- public/assets/js/statistics.js
+++ public/assets/js/statistics.js
@@ -195,7 +195,6 @@ function loadStatisticsData() {
                     doc.addImage(chart_year.getImageURI(), 0, 20, width, height);
                     doc.addPage();
                     doc.addImage(chart_day.getImageURI(), 0, 20, width, height);
-                    doc.addPage();
 
                     doc.save('statistics.pdf');
                 }, false);
