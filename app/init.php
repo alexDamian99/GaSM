fda74805f5b048ebde3aea01eaadaef71e9784ee
StefanBeleuz <stefan.beleuz@yahoo.com>
2020-05-24 20:00:58 +0300

    changes for localhost

--- app/init.php
+++ app/init.php
@@ -1,5 +1,5 @@
 <?php
-// putenv("path_to_public=/tw/GaSM/public");
+putenv("path_to_public=/tw/GaSM/public");
 
 
 require_once("core/App.php");
--- app/models/Database.php
+++ app/models/Database.php
@@ -7,25 +7,25 @@ class Database
 
     private function __construct()
     {
-        // $CONFIG = [
-        //     'servername' => "localhost",
-        //     'username' => "root",
-        //     'password' => '',
-        //     'db' => 'gasm'
-        // ];
-        
-        // $this->conn = new mysqli($CONFIG["servername"], $CONFIG["username"], $CONFIG["password"], $CONFIG["db"]);
-        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $CONFIG = [
+            'servername' => "localhost",
+            'username' => "root",
+            'password' => '',
+            'db' => 'gasm'
+        ];
 
-        $server = $url["host"];
-        $username = $url["user"];
-        $password = $url["pass"];
-        $db = substr($url["path"], 1);
+        $this->conn = new mysqli($CONFIG["servername"], $CONFIG["username"], $CONFIG["password"], $CONFIG["db"]);
+        // $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-        $this->conn = new mysqli($server, $username, $password, $db);
-        if ($this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
-        }
+        // $server = $url["host"];
+        // $username = $url["user"];
+        // $password = $url["pass"];
+        // $db = substr($url["path"], 1);
+
+        // $this->conn = new mysqli($server, $username, $password, $db);
+        // if ($this->conn->connect_error) {
+        //     die("Connection failed: " . $this->conn->connect_error);
+        // }
     }
 
     public static function getInstance()
@@ -42,5 +42,3 @@ class Database
         return $this->conn;
     }
 }
-
-
